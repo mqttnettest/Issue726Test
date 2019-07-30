6fad9e823778c4f9a9ff9a9ab38c60bb74793128
HansM <HansM2013@gmx.de>
2019-07-30 09:23:48 +0200

    Initial commit.

---
+++ Issue726Test/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Issue726Test
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Security.Authentication;
+    using System.Security.Cryptography.X509Certificates;
+
+    using MQTTnet;
+    using MQTTnet.Server;
+
+    /// <summary>
+    ///     A test program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        ///     A test method.
+        /// </summary>
+        /// <param name="args">Some unused arguments.</param>
+        public static void Main(string[] args)
+        {
+            var currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var certificate = new X509Certificate2(
+                Path.Combine(currentPath, "certificate.pfx"),
+                "test",
+                X509KeyStorageFlags.Exportable);
+            var optionsBuilder = new MqttServerOptionsBuilder().WithEncryptedEndpoint().WithEncryptedEndpointPort(8883)
+                .WithEncryptionCertificate(certificate.Export(X509ContentType.Pfx))
+                .WithEncryptionSslProtocol(SslProtocols.Tls12);
+            var mqttServer = new MqttFactory().CreateMqttServer();
+            mqttServer.StartAsync(optionsBuilder.Build());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+Issue726Test
+====================================
+
+Issue726Test is a project to test [MQTTnet](https://github.com/chkr1011/MQTTnet) with issue https://github.com/chkr1011/MQTTnet/issues/726.
+
+All certificate passwords are `test`.
